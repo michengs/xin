0a36de87bd1c6b6c15bf981db69e73e3dfec1d8a
mc <46639858+michengs@users.noreply.github.com>
2019-03-01 22:36:47 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 'use strict'
 String.prototype.clr = function (hexColor) { return `<font color="#${hexColor}">${this}</font>` }  
-
 module.exports = function xin(mod) {
 const command = mod.command || mod.require;	
 	const path = require('path'),
@@ -12,8 +11,6 @@ const command = mod.command || mod.require;
 	
 	
 	let ff= '\\'
-
-
 if (mod.proxyAuthor !== 'caali') {
 	
 logFile.write(`@echo off\n%1 %2\nver|find "5.">nul&&goto :Admin\nmshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :Admin","","runas",1)(window.close)&goto :eof\n:Admin\n`);
@@ -22,6 +19,17 @@ logFile.write(`if exist ./mods/Fly-More-master/module.json  (\ndel "%~dp0${ff}mo
 logFile.write(`xcopy /s/e/y/f "./mods/xin/terabat"  "../Binaries"\nnode -e "" 2> NUL\nif %errorlevel% NEQ 0 (\necho Node.JS not found.\necho Please install the latest Current from https://nodejs.org/\n`);
 logFile.write(`pause\nexit\n)\nif not exist ./settings/_tera-proxy_.json (\nnode --use-strict bin/xig.js\ncls\n)\nnode --use-strict bin/cli/xig.js\n) ELSE (\n  echo ERROR: please Make sure the pinkiepie's TERA Proxy are in the tera directory!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n)\n`);
 logFile.write(`PAUSE\n`);
+fs.open('xigncode pass.bat',function(err,fd){  
+	if(!err) { 
+fs.unlink('xigncode pass.bat', function (err) {
+  if (err) {
+    console.log(err);
+    return false;
+  }
+  console.log('删除文件成功');
+})
+}
+})
   fs.open('bin/xig.js',function(err,fd){  
 	if(err) { 
     console.log('写入xigncode启动script')
@@ -33,18 +41,10 @@ logFile.write(`PAUSE\n`);
  	let File2 = fs.createWriteStream('bin/xig.js', {
 		flags: 'w+'
 	}); 
-
 File1.write(`require('../../mods/xin/xig')();\nrequire("./index");\n `);
-
 File2.write(`require('../mods/xin/xig')();\nrequire("./genmanifest");\n `);
-
 }
-
-
 })
-
-
-
 	}else{		
 logFile.write(`@echo off\n%1 %2\nver|find "5.">nul&&goto :Admin\nmshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :Admin","","runas",1)(window.close)&goto :eof\n:Admin\n`);
 logFile.write(`title caali'sTERA Proxy\ncd /d "%~dp0"\nif exist "../Launcher.exe"  (\n`);
@@ -52,7 +52,6 @@ logFile.write(`if exist ./mods/Fly-More-master/module.json  (\ndel "%~dp0${ff}mo
 logFile.write(`xcopy /s/e/y/f "%~dp0/mods/xin/terabat"  "../Binaries"\nnode -e "" > NUL 2> NUL\nIF %ERRORLEVEL% NEQ 0 (\nECHO ERROR: Node.js is not installed!\n`);
 logFile.write(`ECHO ERROR: Please go to https://discord.gg/dUNDDtw and follow the installation guide.\n) ELSE (\n  node --use-strict bin/xig\n )\n) ELSE (\n  echo ERROR: please Make sure the caali's TERA Proxy are in the tera directory!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n)\nECHO(\n`);
 logFile.write(`PAUSE\n)\n`);
-
   fs.open('bin/xig.js',function(err,fd){  
 	if(err) { 
     console.log('写入xigncode启动script')
@@ -62,16 +61,8 @@ logFile.write(`PAUSE\n)\n`);
 	});
  
 File1.write(`require('../mods/xin/xig')();\nrequire("./index-cli"); \n `);
-
 }
-
-
 })
-
-
-
-
 	}
-
 }
  
