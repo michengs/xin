b7c83d4c24aae1b06e529df30a994826088187ea
mc <46639858+michengs@users.noreply.github.com>
2019-05-04 20:13:19 +0800

    Add files via upload

--- xig/index.js
+++ xig/index.js
@@ -1,105 +1,129 @@
-
 "use strict"
 
 const fs = require("fs");
 const path = require("path");
+const { execFileSync } = require("child_process");
 const ProcessListener = require("./process-listener");
-const scan_interval = require('./config.json');
+const { scan_interval: SCAN_INTERVAL, backup: BACKUP } = require('./config.json');
 
 let PatchedProcesses = {};
+let TmpDir = null;
+let BypassDllPath = null;
 
-function HandleAddedProcess(process) {
-    try {
-        let XigncodeFolder = path.join(path.dirname(process.path), "XIGNCODE");
-        
-        if (!fs.existsSync(path.join(XigncodeFolder, "x3.xem.bak"))) {
-            fs.renameSync(path.join(XigncodeFolder, "x3.xem"), path.join(XigncodeFolder, "x3.xem.bak"));
-            fs.copyFileSync(path.join(__dirname, "res/x3.xem"), path.join(XigncodeFolder, "x3.xem"));
-        }
-        if (!fs.existsSync(path.join(XigncodeFolder, "xcorona.xem.bak"))) {
-            fs.renameSync(path.join(XigncodeFolder, "xcorona.xem"), path.join(XigncodeFolder, "xcorona.xem.bak"));
-            fs.copyFileSync(path.join(__dirname, "res/xcorona.xem"), path.join(XigncodeFolder, "xcorona.xem"));
-        }
-        PatchedProcesses[process.pid] = XigncodeFolder;
+function HandleAddedProcess(process)
+{
+    try
+    {
+        execFileSync(path.join(__dirname, "res/injector.exe"), [process.pid, BypassDllPath]);
+        PatchedProcesses[process.pid] = true;
         console.log(`[xigncode-bypass] TERA游戏客户端 (PID ${process.pid}) 被检测到, bypass 已安装.`);
-    } catch(e) {
-        // Ignore errors...
+    }
+    catch(e)
+    {
+        console.log(`[xigncode-bypass] ERROR: Unable to install bypass (PID ${process.pid})!`);
+        switch(e.code)
+        {
+            case 'ENOENT':
+            {
+                console.log("[xigncode-bypass] injector.exe does not exist. It has likely been deleted by your anti-virus.");
+                console.log("[xigncode-bypass] Disable/uninstall it or whitelist the proxy and injector.exe!");
+                break;
+            }
+            default:
+            {
+                switch(e.status)
+                {
+                    case 1:
+                    {
+                        console.log("[xigncode-bypass] Bypass DLL injection unsuccessful. It has likely been blocked by your anti-virus.");
+                        console.log("[xigncode-bypass] Disable/uninstall it or whitelist the proxy and injector.exe!");
+                        break;
+                    }
+                    default:
+                    {
+                        console.log("[xigncode-bypass] This is likely caused by your anti-virus interfering. Disable/uninstall it or whitelist the proxy.");
+                        console.log("[xigncode-bypass] Full error message:");
+                        console.log(e);
+                        break;
+                    }
+                }
+                break;
+            }
+        }
     }
 }
 
-function HandleRemovedProcess(pid) {
-    try {
-        let XigncodeFolder = PatchedProcesses[pid];
-        
-        if (fs.existsSync(path.join(XigncodeFolder, "x3.xem.bak"))) {
-            fs.renameSync(path.join(XigncodeFolder, "x3.xem.bak"), path.join(XigncodeFolder, "x3.xem"));
-        }
-        if (fs.existsSync(path.join(XigncodeFolder, "xcorona.xem.bak"))) {
-            fs.renameSync(path.join(XigncodeFolder, "xcorona.xem.bak"), path.join(XigncodeFolder, "xcorona.xem"));
-        }
+function HandleRemovedProcess(pid)
+{
+    try
+    {
+     console.log(`[xigncode-bypass] TERA游戏客户端 (PID ${pid}) 已结束.`);
         delete PatchedProcesses[pid];
-     console.log(`[xigncode-bypass] TERA游戏客户端 (PID ${pid}) 已结束, bypass 已还原.`);
-    } catch(e) {
+    }
+    catch(e)
+    {
         // Ignore errors...
     }
 }
 
-ProcessListener("TERA.exe", HandleAddedProcess, HandleRemovedProcess, scan_interval);
+function RandomString(m)
+{
+    // See https://gist.github.com/Dreyer/2368164
+	m = m || 9;
+    const r = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let s = '';
+	for (let i = 0; i < m; i++)
+        s += r.charAt(Math.floor(Math.random()*r.length));
+	return s;
+}
+
+function InitBypassDll()
+{
+    try
+    {
+        TmpDir = path.join(__dirname, "tmp");
+        fs.mkdirSync(TmpDir);
+    }
+    catch(e)
+    {
+        // Ignore errors...
+    }
+
+    BypassDllPath = path.join(TmpDir, RandomString(Math.floor(Math.random() * (20 - 4 + 1)) + 4) + ".dll");
+    fs.copyFileSync(path.join(__dirname, "res/bypass.dll"), BypassDllPath);
+
+    process.on("exit", CleanupBypassDll);
+    process.on("SIGHUP", CleanupBypassDll);
+    process.on("SIGINT", CleanupBypassDll);
+    process.on("SIGTERM", CleanupBypassDll);
+}
+
+function CleanupBypassDll()
+{
+    try
+    {
+        fs.unlinkSync(BypassDllPath);
+        fs.rmdirSync(TmpDir);
+    }
+    catch(e)
+    {
+        // Ignore errors...
+    }
+}
+
+
+    InitBypassDll();
+    ProcessListener("TERA.exe", HandleAddedProcess, HandleRemovedProcess, SCAN_INTERVAL);
 console.log("[bypass] 准备, 等待TERA游戏客户端启动!");
 
-try {
-    fs.unlinkSync("C:\\Windows\\xhunter1.sys");
-    console.log("[bypass] Traces of a previous xigncode installation have been located and removed from your system!");
-    console.log("[bypass] Note that some registry keys might still remain on your system.");
-    console.log("[bypass] Check out #proxy-changelog in https://discord.gg/maqBmJV for instructions on manual removal.");
-} catch(e) {
-    // Ignore errors...
-}
-/*
-const StartupFilePath = [
-    '../bin/index.js',     // Old Proxy
-    '../bin/index-cli.js', // Caali's Proxy
-    '../bin/cli/index.js'  // Pinkie's Proxy
-];
-
-for (let i=0; i < StartupFilePath.length; i++) {
-    if (fs.existsSync(path.join(StartupFilePath[i]))) {
-        module.exports = require(StartupFilePath[i]);
-        return;
+    try
+    {
+        fs.unlinkSync("C:\\Windows\\xhunter1.sys");
+        console.log("[xigncode-bypass] Traces of a previous xigncode installation have been located and removed from your system!");
+        console.log("[xigncode-bypass] Note that some registry keys might still remain on your system.");
+        console.log("[xigncode-bypass] Check out #xigncode-bypass in https://discord.gg/maqBmJV for instructions on manual removal.");
     }
-    
-}
-
-console.log(`注意: TeraProxy版本 不支持 xigncode-bypass!!`);
-
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    catch(e)
+    {
+        // Ignore errors...
+    }
--- xig/process-listener.js
+++ xig/process-listener.js
@@ -4,13 +4,14 @@ const { snapshot } = require("process-list");
 
 let ListedProcesses = {};
 
-module.exports = function ProcessListener(Name, OnAdded, OnRemoved, Interval = 500) {
+module.exports = function ProcessListener(Name, OnAdded, OnRemoved, Interval = 500)
+{
     snapshot("pid", "name", "path").then(processes => {
         let NewListedProcesses = {};
-        for (let process of processes) {
-            if (process.name.toUpperCase() === Name.toUpperCase()) {
-                if (!ListedProcesses[process.pid]) {
-                    if (process.path !== "") {
+        for(let process of processes) {
+            if(process.name.toUpperCase() === Name.toUpperCase()) {
+                if(!ListedProcesses[process.pid]) {
+                    if(process.path !== "") {
                         OnAdded(process);
                         NewListedProcesses[process.pid] = true;
                     }
@@ -20,10 +21,9 @@ module.exports = function ProcessListener(Name, OnAdded, OnRemoved, Interval = 5
             }
         }
         
-        for (let pid in ListedProcesses) {
-            if (!NewListedProcesses[pid]) {
+        for(let pid in ListedProcesses) {
+            if(!NewListedProcesses[pid])
                 OnRemoved(pid);
-            }
         }
         ListedProcesses = NewListedProcesses;
         setTimeout(() => ProcessListener(Name, OnAdded, OnRemoved, Interval), Interval);
