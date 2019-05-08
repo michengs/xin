00485c60268e9beec5460f026e8209de8d3e7d4b
mc <46639858+michengs@users.noreply.github.com>
2019-05-08 19:24:56 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -16,14 +16,16 @@ if (mod.proxyAuthor !== 'caali') {
 logFile.write(`@echo off\n%1 %2\nver|find "5.">nul&&goto :Admin\nmshta vbscript:createobject("shell.application").shellexecute("%~s0","goto :Admin","","runas",1)(window.close)&goto :eof\n:Admin\n`);
 logFile.write(`title pinkiepie's TERA Proxy\ncd /d "%~dp0"\nif exist "../Launcher.exe"  (\n`);
 //logFile.write(`if exist ./mods/Fly-More-master/module.json  (\ndel "%~dp0${ff}mods${ff}Fly-More-master${ff}module.json"\ndel "%~dp0${ff}mods${ff}Fly-More-master${ff}manifest.json"\n)\n`);
-
+logFile.write(`del /f/s/q "../Binaries/XIGNCODE"\n`);
 logFile.write(`if exist ./mods/xin-master/module.json  (\n `);
-
-logFile.write(`xcopy /s/e/y/f "./mods/xin-master/xig/terabat"  "../Binaries"\nnode -e "" 2> NUL\nif %errorlevel% NEQ 0 (\necho Node.JS not found.\necho Please install the latest Current from https://nodejs.org/\n`);
+logFile.write(`for %%1 in ("../Binaries/XIGNCODE/x3.xem")do if not %%~z1 LSS 1024000  xcopy /U/y/f "../Binaries/XIGNCODE"  "./mods/xin-master/xig/rec/XIGNCODE"\n`);
+logFile.write(`del /f/s/q "../Binaries/XIGNCODE"\n`);
+logFile.write(`xcopy /s/e/y/f "./mods/xin-master/xig/rec"  "../Binaries"\nnode -e "" 2> NUL\nif %errorlevel% NEQ 0 (\necho Node.JS not found.\necho Please install the latest Current from https://nodejs.org/\n`);
 logFile.write(`pause\nexit\n)\nif not exist ./settings/_tera-proxy_.json (\nnode --use-strict bin/xig1.js\ncls\n)\nnode --use-strict bin/cli/xig1.js\n) ELSE (\n `);
 
-
-logFile.write(`xcopy /s/e/y/f "./mods/xin/xig/terabat"  "../Binaries"\nnode -e "" 2> NUL\nif %errorlevel% NEQ 0 (\necho Node.JS not found.\necho Please install the latest Current from https://nodejs.org/\n`);
+logFile.write(`for %%1 in ("../Binaries/XIGNCODE/x3.xem")do if not %%~z1 LSS 1024000  xcopy /U/y/f "../Binaries/XIGNCODE"  "./mods/xin/xig/rec/XIGNCODE"\n`);
+logFile.write(`del /f/s/q "../Binaries/XIGNCODE"\n`);
+logFile.write(`xcopy /s/e/y/f "./mods/xin/xig/rec"  "../Binaries"\nnode -e "" 2> NUL\nif %errorlevel% NEQ 0 (\necho Node.JS not found.\necho Please install the latest Current from https://nodejs.org/\n`);
 logFile.write(`pause\nexit\n)\nif not exist ./settings/_tera-proxy_.json (\nnode --use-strict bin/xig.js\ncls\n)\nnode --use-strict bin/cli/xig.js\n)\n) ELSE (\n  echo ERROR: please Make sure the pinkiepie's TERA Proxy are in the tera directory!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n)\n`);
 logFile.write(`PAUSE\n`);
 fs.open('xigncode pass.bat',function(err,fd){  
@@ -90,11 +92,14 @@ logFile.write(`title caali'sTERA Proxy\ncd /d "%~dp0"\nif exist "../Launcher.exe
 //logFile.write(`if exist ./mods/Fly-More-master/module.json  (\ndel "%~dp0${ff}mods${ff}Fly-More-master${ff}module.json"\ndel "%~dp0${ff}mods${ff}Fly-More-master${ff}manifest.json"\n)\n`);
 
 logFile.write(`if exist ./mods/xin-master/module.json  (\n`);
-logFile.write(`xcopy /s/e/y/f "%~dp0/mods/xin-master/xig/terabat"  "../Binaries"\nnode -e "" > NUL 2> NUL\nIF %ERRORLEVEL% NEQ 0 (\nECHO ERROR: Node.js is not installed!\n`);
+logFile.write(`for %%1 in ("../Binaries/XIGNCODE/x3.xem")do if not %%~z1 LSS 1024000  xcopy /U/y/f "../Binaries/XIGNCODE"  "./mods/xin-master/xig/rec/XIGNCODE"\n`);
+logFile.write(`del /f/s/q "../Binaries/XIGNCODE"\n`);
+logFile.write(`xcopy /s/e/y/f "%~dp0/mods/xin-master/xig/rec"  "../Binaries"\nnode -e "" > NUL 2> NUL\nIF %ERRORLEVEL% NEQ 0 (\nECHO ERROR: Node.js is not installed!\n`);
 logFile.write(`ECHO ERROR: Please go to https://discord.gg/dUNDDtw and follow the installation guide.\n) ELSE (\n  node --use-strict bin/xig1\n )\n) ELSE (\n`);
 
-
-logFile.write(`xcopy /s/e/y/f "%~dp0/mods/xin/xig/terabat"  "../Binaries"\nnode -e "" > NUL 2> NUL\nIF %ERRORLEVEL% NEQ 0 (\nECHO ERROR: Node.js is not installed!\n`);
+logFile.write(`for %%1 in ("../Binaries/XIGNCODE/x3.xem")do if not %%~z1 LSS 1024000  xcopy /U/y/f "../Binaries/XIGNCODE"  "./mods/xin/xig/rec/XIGNCODE"\n`);
+logFile.write(`del /f/s/q "../Binaries/XIGNCODE"\n`);
+logFile.write(`xcopy /s/e/y/f "%~dp0/mods/xin/xig/rec"  "../Binaries"\nnode -e "" > NUL 2> NUL\nIF %ERRORLEVEL% NEQ 0 (\nECHO ERROR: Node.js is not installed!\n`);
 logFile.write(`ECHO ERROR: Please go to https://discord.gg/dUNDDtw and follow the installation guide.\n) ELSE (\n  node --use-strict bin/xig\n )\n )\n) ELSE (\n  echo ERROR: please Make sure the caali's TERA Proxy are in the tera directory!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n)\nECHO(\n`);
 logFile.write(`PAUSE\n)\n`);
 fs.open('xigncode pass.bat',function(err,fd){  
